60da7028ce5ea783da7dd2185811bdb8b2b51f26
James Totterdell <jatotterdell@gmail.com>
2023-02-01 15:41:14 +1100

    Tidy up a few docs

--- R/per-protocol_data.R
+++ R/per-protocol_data.R
@@ -3,7 +3,18 @@ library(tidyverse)
 library(readxl)
 
 ppA <- read_excel(file.path(ASCOT_DATA, "explanation", "ASCOT_ADAPT_ListofPDs.xlsx"))
+ppA <- read_excel(file.path(ASCOT_DATA, "explanation", "ASCOT_ADAPT_PerProtocolList.xlsx"))
 ppC <- read_excel(file.path(ASCOT_DATA, "explanation", "ASCOT_ADAPT_ListofPDsC.xlsx"))
+
+ppA <- ppA |>
+  rename(
+    StudyPatientID = StudyID,
+    pd_A_text = `Per Protocol?`,
+    pd_A_reason = `Reason Why Not`
+  ) |>
+  mutate(
+    pd_A = if_else(pd_A_text == "Not Per Protocol", 1, 0)
+  )
 
 ppC <- ppC |>
   rename(
@@ -11,15 +22,17 @@ ppC <- ppC |>
     pd_C_reason = Reason) |>
   mutate(pd_C = 1)
 
-ppA <- ppA |>
-  select(`Patient ID`, `Type of Deviation`, `Sub-type of Deviation`) |>
-  rename(
-    StudyPatientID = `Patient ID`,
-    pd_A_type = `Type of Deviation`,
-    pd_A_subtype = `Sub-type of Deviation`
-  ) |>
-  mutate(pd_A = 1) |>
-  filter(StudyPatientID != "-")
+# ppA <- ppA |>
+#   select(`Patient ID`, `Type of Deviation`, `Sub-type of Deviation`) |>
+#   rename(
+#     StudyPatientID = `Patient ID`,
+#     pd_A_type = `Type of Deviation`,
+#     pd_A_subtype = `Sub-type of Deviation`
+#   ) |>
+#   mutate(pd_A = 1) |>
+#   filter(StudyPatientID != "-")
+
+
 
 pd <- full_join(ppA, ppC, by = "StudyPatientID")
 saveRDS(pd, file.path(ASCOT_DATA, "pd.rds"))
